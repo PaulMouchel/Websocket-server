d61cbaf16800fdcdab6d8ddd610110822db04194
PaulMouchel <paulmouchel@live.fr>
2022-06-13 11:40:55 +0400

    simple websocket server running

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import http from 'http'
+import { Server, Socket } from "socket.io"
+import cors from "cors"
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use(cors())
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    // methods: ["GET", "POST"]
+  }
+})
+
+io.on("connection", (socket: Socket) => {
+  console.log(`User connected: ${socket.id}`)
+
+  socket.on("send_message", (data) => {
+    console.log(data)
+    socket.broadcast.emit("receive_message", data)
+  })
+})
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING")
+})
